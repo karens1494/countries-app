0de4722998b7c49ea0710b5f62cf5c6b505425d8
karens1494 <karen.suarez9414@gmail.com>
2021-08-31 08:24:19 -0600

    empty data message

--- src/components/ListCountries/index.jsx
+++ src/components/ListCountries/index.jsx
@@ -1,10 +1,19 @@
-import { Container, Grid, makeStyles } from "@material-ui/core";
+import { Container, Grid, makeStyles, Typography } from "@material-ui/core";
 
 import CardCountry from "../CardCountry";
 
 const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
+  },
+  empty: {
+    marginTop: 20,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 150,
+    textAlign: "center",
+    width: "100%",
   },
 }));
 
@@ -14,11 +23,17 @@ const ListCountries = ({ countries }) => {
   return (
     <Container>
       <Grid container className={classes.root} spacing={3}>
-        {countries.map((country) => (
-          <Grid key={country.code} item xs={12} sm={6} md={4} lg={3} xl={2}>
-            <CardCountry dataCountry={country} />
-          </Grid>
-        ))}
+        {countries.length > 0 ? (
+          countries.map((country) => (
+            <Grid key={country.code} item xs={12} sm={6} md={4} lg={3} xl={2}>
+              <CardCountry dataCountry={country} />
+            </Grid>
+          ))
+        ) : (
+          <div className={classes.empty}>
+            <Typography variant="h4">No hay Elementos</Typography>
+          </div>
+        )}
       </Grid>
     </Container>
   );
